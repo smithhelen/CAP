816e62e43ef576d905e888b6aa0f01c5b81254fd
HLSmith <h.l.smith@massey.ac.nz>
2022-09-07 11:05:28 +1200

    Explicitly add number of CA axes (k)

--- files/sample_run.R
+++ files/sample_run.R
@@ -68,7 +68,7 @@ table(Prediction) |> as.data.frame() # counts of predictions for each source
 #answer
 
 # 5. cap method, 1 axis
-train <- prepare_training_cap(Dat.train, starts_with("CAMP"), "Source", d=small_d, mp=95, axes=1)
+train <- prepare_training_cap(Dat.train, starts_with("CAMP"), "Source", d=small_d, k=2, mp=95, axes=1)
 test <- prepare_test_cap(Dat.test, train$extra, "LabID")
 rf_mod <- ranger(Source ~ ., data=train$training, oob.error = TRUE, num.trees=500, respect.unordered.factors = TRUE)
 Prediction <- predict(rf_mod, data=test, predict.all = FALSE)$predictions
@@ -79,7 +79,7 @@ table(Prediction) |> as.data.frame() # counts of predictions for each source
 #answer
 
 # 6. cap method, 2 axes
-train <- prepare_training_cap(Dat.train, starts_with("CAMP"), "Source", d=small_d, mp=95, axes=2)
+train <- prepare_training_cap(Dat.train, starts_with("CAMP"), "Source", d=small_d, k=2, mp=95, axes=2)
 test <- prepare_test_cap(Dat.test, train$extra, "LabID")
 rf_mod <- ranger(Source ~ ., data=train$training, oob.error = TRUE, num.trees=500, respect.unordered.factors = TRUE)
 Prediction <- predict(rf_mod, data=test, predict.all = FALSE)$predictions
